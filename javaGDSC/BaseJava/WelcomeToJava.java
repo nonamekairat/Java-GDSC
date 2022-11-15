b3d6dd66f140c36b9925ac0ae4d5e39d96fb7910
nonamekairat <113531357+nonamekairat@users.noreply.github.com>
2022-11-15 08:23:45 +0600

    Add files via upload

---
+++ javaGDSC/BaseJava/WelcomeToJava.java
@@ -0,0 +1,8 @@
+package com.java.se.javaGDSC.BaseJava;
+
+public class WelcomeToJava {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
