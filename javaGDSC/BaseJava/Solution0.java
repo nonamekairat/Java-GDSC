3aaa45dee4cf88f5df92c0562f9feae0d4d314cd
nonamekairat <113531357+nonamekairat@users.noreply.github.com>
2022-11-18 12:06:20 +0600

    Update and rename WelcomeToJava.java to Solution0.java

--- javaGDSC/BaseJava/Solution0.java
+++ javaGDSC/BaseJava/Solution0.java
@@ -1,6 +1,6 @@
 package com.java.se.javaGDSC.BaseJava;
 
-public class WelcomeToJava {
+public class Solution0 {
     public static void main(String[] args) {
         System.out.println("Hello, World.");
         System.out.println("Hello, Java.");
